dd2eb7670924158866771251a5fd504d222f6037
WanBenQAQ <52995164+WanBenQAQ@users.noreply.github.com>
2021-09-20 19:34:32 +0800

    接口修改

--- src/network/request.js
+++ src/network/request.js
@@ -3,9 +3,7 @@ import axios from "axios";
 export function request(config) {
   //1. 创建axios的实例
   const instance = axios.create({
-    //老接口，没新接口好用
-    //baseURL: 'http://123.207.32.32:8000',
-    baseURL: 'http://152.136.185.210:7878/api/m5/',
+    baseURL: 'http://152.136.185.210:7878/api/hy66/',
     timeout: 5000
   })
   //2. axios的拦截器
